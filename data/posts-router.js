99bdaef93882ea20b5260f0b0e77967067592cf9
Zac Smith <zrs3141592@gmail.com>
2019-11-01 20:33:06 -0600

    GET/, working correctly

--- data/posts-router.js
+++ data/posts-router.js
@@ -1,11 +1,51 @@
 const express = require("express");
+const db = require("../data/db.js");
 
 const router = express();
 
+// GET
 router.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Welcome to posts-router"
-  });
+  db.find()
+    .then(posts => {
+      res.status(200).json({
+        message: "Success",
+        posts
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `The post information could not be retrieved, ERROR: ${err}`
+      });
+    });
 });
+
+// GET /:id
+
+// POST
+
+router.post("/", (req, res) => {
+  db.find()
+    .then(posts => {
+      if (!posts.title || !posts.contents) {
+        res.status(400).json({
+          message: `Please provide title and contents for the post`
+        });
+      } else {
+        res.status(200).json({
+          message: "Success",
+          posts
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `There was an error while saving the post to the database. ERROR: ${err}`
+      });
+    });
+});
+
+// PUT /:id
+
+// DELETE /:id
 
 module.exports = router;
