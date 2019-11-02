10bb364910dc8151930475112e0a523e01591ff5
Zac Smith <zrs3141592@gmail.com>
2019-11-01 19:45:30 -0600

    create server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const expres = require("express");
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const server = express();
+
+server.use(helmet());
+server.use(morgan("dev"));
+server.use(express.json());
+server.use("/api/");
 
 server.get("/", (req, res) => {
   res.status(200).json({
